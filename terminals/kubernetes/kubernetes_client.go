79825e2f3a97d6acf76c3bc74eab0181738797bd
lij138 <lij138@bngrp.com>
2021-05-10 03:41:14 -0400

    tt

--- terminals/kubernetes/kubernetes_client.go
+++ terminals/kubernetes/kubernetes_client.go
@@ -10,27 +10,6 @@ import (
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/tools/remotecommand"
 	"strings"
-	"time"
-)
-
-const (
-	// Time allowed to write a message to the peer.
-	writeWait = 10 * time.Second
-
-	// Maximum message size allowed from peer.
-	maxMessageSize = 8192
-
-	// Time allowed to read the next pong message from the peer.
-	pongWait = 60 * time.Second
-
-	// Send pings to peer with this period. Must be less than pongWait.
-	pingPeriod = (pongWait * 9) / 10
-
-	// Time to wait before force close on connection.
-	closeGracePeriod = 10 * time.Second
-
-	// EndOfTransmission end
-	EndOfTransmission = "\u0004"
 )
 
 // PtyHandler is what remotecommand expects from a pty
@@ -103,8 +82,6 @@ func (t *TerminalSession) Next() *remotecommand.TerminalSize {
 		return &size
 	case <-t.doneChan:
 		return nil
-	default:
-		return nil
 	}
 }
 
@@ -143,6 +120,7 @@ func (t *TerminalSession) Read(p []byte) (int, error) {
 	_, message, err := t.wsConn.ReadMessage()
 	if err != nil {
 		log.Error("Read websocket message error by",err)
+		t.Close()
 		return 0, nil
 	}
 	cmd := string(message)
@@ -165,6 +143,7 @@ func (t *TerminalSession) Read(p []byte) (int, error) {
 func (t *TerminalSession) Write(p []byte) (int, error) {
 	if err := t.wsConn.WriteMessage(websocket.TextMessage, p); err != nil {
 		log.Warnf("write message err: %v \n", err)
+		t.Close()
 		return 0, err
 	}
 	return len(p), nil
